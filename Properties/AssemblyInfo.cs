cce50e2d144b40791931c43da83331889576b357
lltcggie <7032792+lltcggie@users.noreply.github.com>
2023-12-30 23:35:28 +0900

    AssemblyInfo.cs追加

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DspFontPatcher")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DspFontPatcher")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("81DF3045-8007-4F6A-AAF6-903139D85B6C")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
